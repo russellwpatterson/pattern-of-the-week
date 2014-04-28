77ca9996b4132ce747b624376def17e430227332
Russell Patterson <rwp.cpe@gmail.com>
2014-04-27 21:10:53 -0400

    Builder pattern example.

---
+++ builder/Builder/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Drawing;
+
+namespace Builder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ShirtBuilder();
+
+            builder.ShirtColor = Color.Blue;
+            builder.ShirtSize = 45;
+
+            var shirt = builder.Build();
+
+            Console.WriteLine("Color: " + shirt.Color.ToString());
+            Console.WriteLine("Size: " + shirt.Size.ToString());
+
+            Console.Read();
+        }
+    }
+}
---
+++ builder/Builder/ShirtBuilder.cs
@@ -0,0 +1,45 @@
+﻿using System.Drawing;
+
+namespace Builder
+{
+    public interface IShirtBuilder
+    {
+        Color ShirtColor { get; set; }
+        int ShirtSize { get; set; }
+        ShirtBuilder.IShirt Build();
+    }
+
+    public class ShirtBuilder : IShirtBuilder
+    {
+        public ShirtBuilder()
+        {
+            this.ShirtColor = Color.Black;
+            this.ShirtSize = 1;
+        }
+
+        public Color ShirtColor { get; set; }
+        public int ShirtSize { get; set; }
+
+        public IShirt Build()
+        {
+            // We could create any type here that implements IShirt.
+            var shirt = new Shirt();
+            shirt.Color = this.ShirtColor;
+            shirt.Size = this.ShirtSize;
+
+            return shirt;
+        }
+
+        public interface IShirt
+        {
+            Color Color { get; set; }
+            int Size { get; set; }
+        }
+
+        private class Shirt : IShirt
+        {
+            public Color Color { get; set; }
+            public int Size { get; set; }
+        }
+    }
+}
